d76bc88f7dcb471d13b7d94e90cd3addca0f8838
Tomking274 <48964332+Tomking274@users.noreply.github.com>
2019-03-27 15:59:20 -0400

    Add files via upload

---
+++ money-check.py
@@ -0,0 +1,8 @@
+a = input("Bank Account Balance:")
+int_a = float(a)
+if int_a >= 1:
+    print("You have money.")
+elif int_a == 0:
+    print("You have no money!")
+else:
+    print("You are in debt!!!")
